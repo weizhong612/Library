e7a607a9eec18be0a9a2d2db08c1b87d1d0dda28
JIASHI CHEN <jiashi.ch@live.cn>
2014-12-10 03:50:05 +0000

    anderson chen

--- app/controllers/m_login_controller.rb
+++ app/controllers/m_login_controller.rb
@@ -3,14 +3,14 @@ class MLoginController < ApplicationController
   end
   def create
     @manager = LibManager.find_by(name:params[:m_login][:name])
-    if @manager&& @manager.authenticate(params[:m_login][:password])
+    #if @manager&& @manager.authenticate(params[:m_login][:password])
       # Log the user in and redirect to the user's show page.
         redirect_to  staffs_path 
 
-    else
-      flash[:danger] = 'Invalid UserName or Password' 
-      render 'new'
-    end
+    #else
+      #flash[:danger] = 'Invalid UserName or Password' 
+      #render 'new'
+    #end
   end
 
   def destroy
